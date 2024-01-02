555bf1c945a315e005aed4b2bcf4df00986b6881
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-01-02 19:54:15 +0100

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.scss";
+import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
 import {dark} from "@clerk/themes";
 import {ThemeProvider} from "@/components/theme-provider";
