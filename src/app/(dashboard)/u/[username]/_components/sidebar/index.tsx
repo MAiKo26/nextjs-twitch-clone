047eb5ee21dff61c674b9689d1d9f3b82d59b7d7
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-01-03 17:45:33 +0100

    chat settings

--- src/app/(dashboard)/u/[username]/_components/sidebar/index.tsx
+++ src/app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import {Toggle} from "./toggle";
+import {Toggle} from "./Toggle";
 import {Wrapper} from "./wrapper";
 import {Navigation} from "./navigation";
 
---
+++ src/app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,48 @@
+import {getSelf} from "@/lib/auth-service";
+import {getStreamByUserId} from "@/lib/stream-service";
+
+import {ToggleCard} from "./_components/toggle-card";
+import {toast} from "sonner";
+
+const ChatPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    // toast.error("Stream not found");
+    return (
+      <div className="p-6">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">Stream not found</h1>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Chat settings</h1>
+      </div>
+      <div className="space-y-4">
+        <ToggleCard
+          field="isChatEnabled"
+          label="Enable chat"
+          value={stream.isChatEnabled}
+        />
+        <ToggleCard
+          field="isChatDelayed"
+          label="Delay chat"
+          value={stream.isChatDelayed}
+        />
+        <ToggleCard
+          field="isChatFollowersOnly"
+          label="Must be following to chat"
+          value={stream.isChatFollowersOnly}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
